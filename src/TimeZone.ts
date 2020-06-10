342a12b09035a8634c574b175d4b58d76ec5dca2
Mark Tucker <mark@shazaml.com>
2020-06-10 07:13:09 -0700

    Break out plugin core code into TimeZone class.

---
+++ src/TimeZone.ts
@@ -0,0 +1,70 @@
+import { Jovo } from 'jovo-core';
+import type { BixbyRequest } from 'jovo-platform-bixby';
+import { AlexaSkill } from 'jovo-platform-alexa';
+
+export class TimeZone {
+    jovo: Jovo;
+    constructor(jovo: Jovo) {
+      this.jovo = jovo;
+    }
+    async getTimeZone() {
+
+      const platformType = this.jovo.getType();
+
+      if (platformType === 'BixbyCapsule') {
+        return (this.jovo.$request! as BixbyRequest).vivContext!.timezone;
+      }
+
+      if (this.jovo.$session.$data.timeZone) {
+        return this.jovo.$session.$data.timeZone;
+      }
+
+      if (platformType === 'AlexaSkill') {
+        try {
+          const result = await (this.jovo.$alexaSkill! as AlexaSkill).$user.getTimezone();
+          this.jovo.$session.$data.timeZone = result;
+        } catch (error) {
+          this.jovo.$session.$data.timeZone = this.getDefaultTimeZone();
+        }
+
+        return this.jovo.$session.$data.timeZone;
+      }
+
+      if (platformType === 'GoogleAction') {
+        // TODO: Use defaults until better Time Zone support
+        this.jovo.$session.$data.timeZone = this.getDefaultTimeZone();
+
+        return this.jovo.$session.$data.timeZone;
+      }
+
+      return this.getDefaultTimeZone();
+    }
+
+    getDefaultTimeZone() {
+
+      const pluginConfig = this.jovo.$app.$plugins.get('TimeZonePlugin')!.config;
+
+      if (!this.jovo) {
+        return pluginConfig!.defaultTimeZone;
+      }
+
+      const locale = this.jovo.getLocale();
+      if (!locale) {
+        return pluginConfig!.defaultTimeZone;
+      }
+
+      const parts = locale.split('-');
+      const countryCode = parts[parts.length - 1];
+
+      if (!countryCode) {
+        return pluginConfig!.defaultTimeZone;
+      }
+
+      const timezone = pluginConfig!.defaultByLocaleCountryCode[countryCode];
+      if (timezone) {
+        return timezone;
+      }
+
+      return pluginConfig!.defaultTimeZone;
+    }
+  }
--- src/TimeZonePlugin.ts
+++ src/TimeZonePlugin.ts
@@ -1,8 +1,15 @@
-import { BaseApp, HandleRequest, Jovo, Plugin, PluginConfig } from 'jovo-core';
+import { BaseApp, HandleRequest, Plugin, PluginConfig } from 'jovo-core';
+import { TimeZone } from './TimeZone';
 
 export interface Config extends PluginConfig {
   defaultTimeZone?: string,
   defaultByLocaleCountryCode?: object,
+}
+
+declare module 'jovo-core/dist/src/core/Jovo' {
+  export interface Jovo {
+    $timeZone: TimeZone;
+  }
 }
 
 export class TimeZonePlugin implements Plugin {
@@ -10,96 +17,25 @@ export class TimeZonePlugin implements Plugin {
     defaultTimeZone: 'America/New_York'
   };
 
-  private _jovo?: Jovo;
 
   constructor(config?: Config) {
     if (config) {
-        this.config = {
-            ...this.config,
-            ...config,
-        };
+      this.config = {
+        ...this.config,
+        ...config,
+      };
     }
-    // this._jovo = null;
   }
 
   install(app: BaseApp) {
-    Jovo.prototype.$timeZone = this;
-
     app.middleware('after.platform.init')!.use(this.initHandler.bind(this));
-
   }
 
   initHandler(handleRequest: HandleRequest) {
     const { jovo } = handleRequest;
-
     if (!jovo) {
-        return;
+      return;
     }
-
-    this._jovo = jovo;
+    jovo.$timeZone = new TimeZone(jovo);
   }
-
-  async getTimeZone() {
-    if (!this._jovo) {
-      return this.getDefaultTimeZone();
-    }
-
-    const platformType = this._jovo.getType();
-
-    if (platformType === 'BixbyCapsule') {
-      return this._jovo.$request.vivContext.timezone;
-    }
-
-    if (this._jovo.$session.$data.timeZone) {
-      return this._jovo.$session.$data.timeZone;
-    }
-
-    if (platformType === 'AlexaSkill') {
-      try {
-        const result = await this._jovo.$alexaSkill.$user.getTimezone();
-        this._jovo.$session.$data.timeZone = result;
-      } catch (error) {
-        this._jovo.$session.$data.timeZone = this.getDefaultTimeZone();
-      }
-
-      return this._jovo.$session.$data.timeZone;
-    }
-
-    if (platformType === 'GoogleAction') {
-      // TODO: Use defaults until better Time Zone support
-      this._jovo.$session.$data.timeZone = this.getDefaultTimeZone();
-
-      return this._jovo.$session.$data.timeZone;
-    }
-
-    return this.getDefaultTimeZone();
-  }
-
-  getDefaultTimeZone() {
-    if (!this._jovo) {
-      return this.config.defaultTimeZone;
-    }
-
-    const locale = this._jovo.getLocale();
-    if (!locale) {
-      return this.config.defaultTimeZone;
-    }
-
-    const parts = locale.split('-');
-    const countryCode = parts[parts.length - 1];
-
-    if (!countryCode) {
-      return this.config.defaultTimeZone;
-    }
-
-    const timezone = this.config.defaultByLocaleCountryCode[countryCode];
-    if (timezone) {
-      return timezone;
-    }
-
-    return this.config.defaultTimeZone;
-  }
-
 }
-
-exports.TimeZonePlugin = TimeZonePlugin;
