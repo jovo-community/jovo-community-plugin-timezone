7f19a7725cb73a93e4fb5bd8768e62b5386248ba
Jan König <jankoenig120190@googlemail.com>
2022-05-04 09:45:04 +0200

    🚧 Add first version of v4 plugin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-export { TimeZonePlugin } from "./TimeZonePlugin";
+import { JovoTimeZone } from './JovoTimeZone';
+import { TimeZonePlugin, TimeZonePluginConfig } from './TimeZonePlugin';
+
+declare module '@jovotech/framework/dist/types/Extensible' {
+  interface ExtensiblePluginConfig {
+    TimeZonePlugin?: TimeZonePluginConfig;
+  }
+
+  interface ExtensiblePlugins {
+    TimeZonePlugin?: TimeZonePlugin;
+  }
+}
+
+declare module '@jovotech/framework/dist/types/Jovo' {
+  interface Jovo {
+    $timeZone: JovoTimeZone;
+  }
+}
+
+export * from './TimeZonePlugin';
