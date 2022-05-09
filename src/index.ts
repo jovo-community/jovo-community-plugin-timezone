fceaf6f21c98f88ec043e6720075d97029c51e3d
Jan König <jankoenig120190@googlemail.com>
2022-05-09 15:41:48 +0200

    🐛 Fix type issues

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { JovoTimeZone } from './JovoTimeZone';
 import { TimeZonePlugin, TimeZonePluginConfig } from './TimeZonePlugin';
 
+import type {Alexa} from '@jovotech/platform-alexa';
+import type {GoogleAssistant} from '@jovotech/platform-googleassistant'; 
+
 declare module '@jovotech/framework/dist/types/Extensible' {
   interface ExtensiblePluginConfig {
     TimeZonePlugin?: TimeZonePluginConfig;
